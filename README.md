217feb4f64966f395d1de52d79ea7e7d8cde7175
Jason Moore <jason@zivtech.com>
2019-09-06 17:55:41 -0400

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jira Service Desk Widget
+A Drupal 8 module that adds a configurable custom block for a Jira Service Desk Widget.
---
+++ src/Plugin/Block/WidgetBlock.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\jsd_widget\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a Jira Service Desk Widget Block.
+ *
+ * @Block(
+ *   id = "widget_block",
+ *   admin_label = @Translation("Widget block"),
+ *   category = @Translation("Hello Widget"),
+ * )
+ */
+class WidgetBlock extends BlockBase implements BlockPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['widget_data_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Data Key'),
+      '#description' => $this->t('The data-key value from the JSD Widget settings.'),
+      '#default_value' => isset($config['widget_data_key']) ? $config['widget_data_key'] : '',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $values = $form_state->getValues();
+    $this->configuration['widget_data_key'] = $values['widget_data_key'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+  	$data_key = $config['widget_data_key'];
+    return [
+      '#markup' => $this->t('<script data-jsd-embedded data-key="' . $data_key . '" data-base-url="https://jsd-widget.atlassian.com" src="https://jsd-widget.atlassian.com/assets/embed.js"></script>'),
+    ];
+  }
+
+}
